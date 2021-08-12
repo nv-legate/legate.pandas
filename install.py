e60305d17fcd19abbdbaf83b17d7d759c5f97d71
Manolis Papadakis <mpapadakis@nvidia.com>
2021-08-11 17:17:38 -0700

    CMake always has a default now

--- install.py
+++ install.py
@@ -133,17 +133,6 @@ def install_thrust(thrust_dir):
     )
 
 
-def get_cmake_config(cmake, legate_dir, default=None):
-    config_filename = os.path.join(legate_dir, ".cmake.json")
-    if cmake is None:
-        cmake = load_json_config(config_filename)
-        if cmake is None:
-            cmake = default
-    assert cmake in [True, False]
-    dump_json_config(config_filename, cmake)
-    return cmake
-
-
 def find_c_define(define, header):
     with open(header, "r") as f:
         line = f.readline()
@@ -292,7 +281,8 @@ def install(
 ):
     legate_pandas_dir = os.path.dirname(os.path.realpath(__file__))
 
-    cmake = get_cmake_config(cmake, legate_pandas_dir, default=False)
+    cmake_config = os.path.join(legate_pandas_dir, ".cmake.json")
+    dump_json_config(cmake_config, cmake)
 
     if thread_count is None:
         thread_count = multiprocessing.cpu_count()
